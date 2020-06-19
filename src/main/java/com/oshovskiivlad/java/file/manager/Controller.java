7899114a6a823dfa689d1e0c981e22c00437d0ff
Vlad <oshovsky.vlad@yandex.ru>
2020-06-19 18:54:47 +0800

    First commit

---
+++ src/main/java/com/oshovskiivlad/java/file/manager/Controller.java
@@ -0,0 +1,16 @@
+package com.oshovskiivlad.java.file.manager;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Controller {
+}
